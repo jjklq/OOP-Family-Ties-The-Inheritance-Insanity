9dca1ed6e158b6d605bfbd7176e91e3d1d546a20
jjklq <148258631+jjklq@users.noreply.github.com>
2023-11-07 17:47:24 +0200

    Add files via upload

---
+++ OOP_Family_Ties.py
@@ -0,0 +1,49 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+class Book(Product):
+    def __init__(self, name, price, quantity, author):
+        super().__init__(name, price, quantity)
+        self.author = author
+    
+    def read(self):
+        print(f"Book: {self.name}, Author: {self.author}, Price: ${self.price}, Quantity: {self.quantity}")
+
+class Restaurant:
+    def __init__(self, name, cuisine, menu):
+        self.name = name
+        self.cuisine = cuisine
+        self.menu = menu
+
+class FastFood(Restaurant):
+    def __init__(self, name, cuisine, menu, drive_thru):
+        super().__init__(name, cuisine, menu)
+        self.drive_thru = drive_thru
+
+    def order(self, dish_name, quantity):
+        if dish_name in self.menu:
+            dish = self.menu[dish_name]
+            if quantity <= dish['quantity']:
+                total_cost = dish['price'] * quantity
+                dish['quantity'] -= quantity
+                return total_cost
+            else:
+                return "Requested quantity not available"
+        else:
+            return "Dish not available"
+
+# Example of usage
+menu = {
+    'burger': {'price': 5, 'quantity': 10},
+    'pizza': {'price': 10, 'quantity': 20},
+    'drink': {'price': 1, 'quantity': 15}
+}
+
+mc = FastFood('McDonalds', 'Fast Food', menu, True)
+
+print(mc.order('burger', 5))  # Output: 25
+print(mc.order('burger', 15))  # Output: Requested quantity not available
+print(mc.order('soup', 5))  # Output: Dish not available
